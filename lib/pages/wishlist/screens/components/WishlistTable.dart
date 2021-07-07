7c51c3cfc03567ec52418aaa87f02a2d92b66a37
CJSZ01 <56030569+CJSZ01@users.noreply.github.com>
2021-07-06 21:34:03 -0300

    implemented scroll

--- lib/pages/wishlist/screens/components/WishlistTable.dart
+++ lib/pages/wishlist/screens/components/WishlistTable.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:consumatron/common/enums.dart';
+
 import 'package:flutter/material.dart';
 
 import 'UpdateWishlistItemDialog.dart';
@@ -42,104 +42,109 @@ class _WishlistTableState extends State<WishlistTable> {
           return Text("Loading");
         }
 
-        return DataTable(
-          showCheckboxColumn: false,
-          dataRowColor: MaterialStateProperty.all(Colors.black54),
-          dataTextStyle:
-              TextStyle(color: Theme.of(context).colorScheme.primary),
-          columns: [
-            DataColumn(
-              onSort: (columnIndex, ascending) {
-                setState(() {
-                  order = 'name';
-                  descending = !descending;
-                });
-              },
-              label: Text(
-                'name',
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ),
-            DataColumn(
-              onSort: (columnIndex, ascending) {
-                setState(() {
-                  order = 'cost';
-                  descending = !descending;
-                });
-              },
-              numeric: true,
-              label: Text(
-                'cost',
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ),
-            DataColumn(
-              onSort: (columnIndex, ascending) {
-                setState(() {
-                  order = 'priority';
-                  descending = !descending;
-                });
-              },
-              numeric: true,
-              label: Text(
-                'priority',
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ),
-            DataColumn(
-              onSort: (columnIndex, ascending) {
-                setState(() {
-                  order = 'date';
-                  descending = !descending;
-                });
-              },
-              numeric: true,
-              label: Text(
-                'date added',
-                style: Theme.of(context).textTheme.bodyText2,
-              ),
-            ),
-          ],
-          rows: snapshot.data!.docs.map((DocumentSnapshot documents) {
-            Map<String, dynamic> data =
-                documents.data() as Map<String, dynamic>;
-            var creationDate = data['date'].toDate();
-            return DataRow(
-              cells: [
-                DataCell(
-                  Text(
-                    data['name'],
+        return Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: DataTable(
+              showCheckboxColumn: false,
+              dataRowColor: MaterialStateProperty.all(Colors.black54),
+              dataTextStyle:
+                  TextStyle(color: Theme.of(context).colorScheme.primary),
+              columns: [
+                DataColumn(
+                  onSort: (columnIndex, ascending) {
+                    setState(() {
+                      order = 'name';
+                      descending = !descending;
+                    });
+                  },
+                  label: Text(
+                    'name',
                     style: Theme.of(context).textTheme.bodyText2,
                   ),
                 ),
-                DataCell(
-                  Text(
-                    data['cost'],
+                DataColumn(
+                  onSort: (columnIndex, ascending) {
+                    setState(() {
+                      order = 'cost';
+                      descending = !descending;
+                    });
+                  },
+                  numeric: true,
+                  label: Text(
+                    'cost',
                     style: Theme.of(context).textTheme.bodyText2,
                   ),
                 ),
-                DataCell(
-                  Text(
-                    data['priority'],
+                DataColumn(
+                  onSort: (columnIndex, ascending) {
+                    setState(() {
+                      order = 'priority';
+                      descending = !descending;
+                    });
+                  },
+                  numeric: true,
+                  label: Text(
+                    'priority',
                     style: Theme.of(context).textTheme.bodyText2,
                   ),
                 ),
-                DataCell(
-                  Text(
-                    creationDate.toString(),
+                DataColumn(
+                  onSort: (columnIndex, ascending) {
+                    setState(() {
+                      order = 'date';
+                      descending = !descending;
+                    });
+                  },
+                  numeric: true,
+                  label: Text(
+                    'date added',
                     style: Theme.of(context).textTheme.bodyText2,
                   ),
                 ),
               ],
-              onSelectChanged: (value) {
-                if (value!) {
-                  //Why does this work???
+              rows: snapshot.data!.docs.map((DocumentSnapshot documents) {
+                Map<String, dynamic> data =
+                    documents.data() as Map<String, dynamic>;
+                var creationDate = data['date'].toDate();
+                return DataRow(
+                  cells: [
+                    DataCell(
+                      Text(
+                        data['name'],
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                    DataCell(
+                      Text(
+                        data['cost'],
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                    DataCell(
+                      Text(
+                        data['priority'],
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                    DataCell(
+                      Text(
+                        creationDate.toString(),
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ),
+                  ],
+                  onSelectChanged: (value) {
+                    if (value!) {
+                      //Why does this work???
 
-                  _showUpdateWishlistItemAlertDialog(documents.id);
-                }
-              },
-            );
-          }).toList(),
+                      _showUpdateWishlistItemAlertDialog(documents.id);
+                    }
+                  },
+                );
+              }).toList(),
+            ),
+          ),
         );
       },
     );
